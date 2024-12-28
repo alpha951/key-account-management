8d8b17dcceddcd34a218c8c1a4b2fa864fc391fe
Keshav <keshav950personal@gmail.com>
2024-12-28 16:38:05 +0530

    Update schema.sql

--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -1,166 +1,169 @@
-CREATE TABLE "kam_user"
-(
-    "id"          BIGINT                      NOT NULL PRIMARY KEY,
-    "mobile"      VARCHAR(255)                NOT NULL,
-    "name"        VARCHAR(255)                NOT NULL,
-    "password"    VARCHAR(255)                NOT NULL,
-    "role"        BIGINT                      NOT NULL,
-    "employee_id" VARCHAR(255)                NOT NULL,
-    "email"       VARCHAR(255)                NOT NULL,
-    "is_active"   BOOLEAN                     NOT NULL,
-    "created_at"  TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updated_at"  TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
+-- PostgreSQL database schema
 
-CREATE TABLE "restaurant"
+-- Table: kam_user
+CREATE TABLE public.kam_user
 (
-    "id"              BIGINT                      NOT NULL PRIMARY KEY,
-    "restaurant_name" VARCHAR(255)                NOT NULL,
-    "pin_code"        VARCHAR(255)                NOT NULL,
-    "created_by"      BIGINT                      NOT NULL,
-    "city"            VARCHAR(255)                NOT NULL,
-    "state"           VARCHAR(255)                NOT NULL,
-    "address"         VARCHAR(255)                NOT NULL,
-    "created_at"      TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updated_at"      TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT "restaurant_created_by_foreign" FOREIGN KEY ("created_by") REFERENCES "kam_user" ("id")
-);
-
-CREATE TABLE "lead"
-(
-    "lead_id"       BIGINT                      NOT NULL PRIMARY KEY,
-    "restaurant_id" BIGINT                      NOT NULL,
-    "created_by"    BIGINT                      NOT NULL,
-    "lead_status"   BIGINT                      NOT NULL,
-    "created_at"    TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updated_at"    TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT "lead_restaurant_id_foreign" FOREIGN KEY ("restaurant_id") REFERENCES "restaurant" ("id"),
-    CONSTRAINT "lead_created_by_foreign" FOREIGN KEY ("created_by") REFERENCES "kam_user" ("id")
-);
-
-CREATE TABLE "restaurant_poc"
-(
-    "poc_id"        BIGINT                      NOT NULL PRIMARY KEY,
-    "name"          VARCHAR(255)                NOT NULL,
-    "restaurant_id" BIGINT                      NOT NULL,
-    "poc_role"      BIGINT                      NOT NULL,
-    "contact"       VARCHAR(255)                NOT NULL,
-    "created_by"    BIGINT                      NOT NULL,
-    "created_at"    TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updated_at"    TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT "restaurant_poc_restaurant_id_foreign" FOREIGN KEY ("restaurant_id") REFERENCES "restaurant" ("id"),
-    CONSTRAINT "restaurant_poc_created_by_foreign" FOREIGN KEY ("created_by") REFERENCES "kam_user" ("id")
-);
-
-CREATE TABLE "call_schedule"
-(
-    "id"                  BIGINT                      NOT NULL PRIMARY KEY,
-    "lead_id"             BIGINT                      NOT NULL,
-    "poc_id"              BIGINT                      NOT NULL,
-    "call_frequency"      BIGINT                      NOT NULL,
-    "call_frequency_type" VARCHAR(50)                 NOT NULL DEFAULT 'MONTHLY',
-    "call_timing"         TIMESTAMP WITH TIME ZONE    NULL,
-    "last_call"           TIMESTAMP WITH TIME ZONE    NULL,
-    "created_at"          TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updated_at"          TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT "call_schedule_lead_id_foreign" FOREIGN KEY ("lead_id") REFERENCES "lead" ("lead_id"),
-    CONSTRAINT "call_schedule_poc_id_foreign" FOREIGN KEY ("poc_id") REFERENCES "restaurant_poc" ("poc_id")
-);
-
-CREATE TABLE "interaction"
-(
-    "id"                  BIGINT                      NOT NULL PRIMARY KEY,
-    "caller"              BIGINT                      NOT NULL,
-    "lead_id"             BIGINT                      NOT NULL,
-    "restaurant_id"       BIGINT                      NOT NULL,
-    "poc_id"              BIGINT                      NOT NULL,
-    "call_schedule_id"    BIGINT                      NULL,
-    "call_duration"       BIGINT                      NOT NULL,
-    "interaction_details" TEXT                        NOT NULL,
-    "interaction_type"    VARCHAR(50)                 NOT NULL,
-    "created_at"          TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT "interaction_caller_foreign" FOREIGN KEY ("caller") REFERENCES "kam_user" ("id"),
-    CONSTRAINT "interaction_lead_id_foreign" FOREIGN KEY ("lead_id") REFERENCES "lead" ("lead_id"),
-    CONSTRAINT "interaction_restaurant_id_foreign" FOREIGN KEY ("restaurant_id") REFERENCES "restaurant" ("id"),
-    CONSTRAINT "interaction_poc_id_foreign" FOREIGN KEY ("poc_id") REFERENCES "restaurant_poc" ("poc_id"),
-    CONSTRAINT "interaction_call_schedule_id_foreign" FOREIGN KEY ("call_schedule_id") REFERENCES "call_schedule" ("id")
-);
-
-CREATE TABLE "order"
-(
-    "id"                  BIGINT                      NOT NULL PRIMARY KEY,
-    "order_id"            UUID                        NOT NULL,
-    "lead_id"             BIGINT                      NOT NULL,
-    "restaurant_id"       BIGINT                      NOT NULL,
-    "interaction_id"      BIGINT                      NOT NULL,
-    "restaurant_order_id" VARCHAR(255)                NOT NULL,
-    "amount"              BIGINT                      NOT NULL,
-    "currency"            VARCHAR(50)                 NOT NULL,
-    "cart_info"           TEXT                        NOT NULL,
-    "shipping_info"       TEXT                        NOT NULL,
-    "offer_details"       TEXT                        NOT NULL,
-    "created_by"          BIGINT                      NOT NULL,
-    "payment_methods"     VARCHAR(50)                 NOT NULL,
-    "remarks"             VARCHAR(255)                NOT NULL,
-    "created_at"          TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT "order_lead_id_foreign" FOREIGN KEY ("lead_id") REFERENCES "lead" ("lead_id"),
-    CONSTRAINT "order_restaurant_id_foreign" FOREIGN KEY ("restaurant_id") REFERENCES "restaurant" ("id"),
-    CONSTRAINT "order_interaction_id_foreign" FOREIGN KEY ("interaction_id") REFERENCES "interaction" ("id"),
-    CONSTRAINT "order_created_by_foreign" FOREIGN KEY ("created_by") REFERENCES "kam_user" ("id")
-);
-
-CREATE TABLE "lead_perf_matrics"
-(
-    "id"                BIGINT                      NOT NULL PRIMARY KEY,
-    "lead_id"           BIGINT                      NOT NULL,
-    "avg_order_value"   BIGINT                      NOT NULL,
-    "order_freq"        BIGINT                      NOT NULL,
-    "total_orders"      BIGINT                      NOT NULL,
-    "interaction_count" BIGINT                      NOT NULL,
-    "total_revenue"     BIGINT                      NOT NULL,
-    "last_order_date"   TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "created_at"        TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updated_at"        TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT "lead_perf_matrics_lead_id_foreign" FOREIGN KEY ("lead_id") REFERENCES "lead" ("lead_id")
-);
-
-CREATE TABLE "kam_system_metrics"
-(
-    "id"                BIGINT      NOT NULL PRIMARY KEY,
-    "metric_name"       BIGINT      NOT NULL,
-    "metric_value"      BIGINT      NOT NULL,
-    "metric_value_type" BIGINT      NOT NULL,
-    "timeframe"         VARCHAR(50) NOT NULL,
-    "year"              BIGINT      NOT NULL,
-    "month"             BIGINT      NOT NULL,
-    "day"               BIGINT      NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS "kam_change_log"
-(
-    "change_id"   BIGINT PRIMARY KEY,
-    "lead_id"     BIGINT                      NOT NULL,
-    "old_kam_id"  BIGINT,
-    "new_kam_id"  BIGINT,
-    "change_date" TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP
+    id          BIGSERIAL PRIMARY KEY,
+    mobile      VARCHAR(255)                                          NOT NULL,
+    name        VARCHAR(255)                                          NOT NULL,
+    password    VARCHAR(255)                                          NOT NULL,
+    role        BIGINT                                                NOT NULL,
+    employee_id VARCHAR(255)                                          NOT NULL,
+    email       VARCHAR(255)                                          NOT NULL,
+    is_active   BOOLEAN                                               NOT NULL,
+    created_at  TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at  TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    UNIQUE (email),
+    UNIQUE (employee_id),
+    UNIQUE (mobile)
 );
 
 
-ALTER TABLE public.kam_user ADD CONSTRAINT kam_user_unique_mobile UNIQUE (mobile);
-ALTER TABLE public.kam_user ADD CONSTRAINT kam_user_unique_email UNIQUE (email);
-ALTER TABLE public.kam_user ADD CONSTRAINT kam_user_unique_employee UNIQUE (employee_id);
+-- Table: restaurant
+CREATE TABLE public.restaurant
+(
+    id              BIGSERIAL PRIMARY KEY,
+    restaurant_name VARCHAR(255)                                          NOT NULL,
+    pincode         VARCHAR(255)                                          NOT NULL,
+    created_by      BIGINT                                                NOT NULL,
+    city            VARCHAR(255)                                          NOT NULL,
+    state           VARCHAR(255)                                          NOT NULL,
+    address         VARCHAR(255)                                          NOT NULL,
+    created_at      TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at      TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    FOREIGN KEY (created_by) REFERENCES public.kam_user (id)
+);
 
 
+-- Table: restaurant_poc
+CREATE TABLE public.restaurant_poc
+(
+    poc_id        BIGSERIAL PRIMARY KEY,
+    name          VARCHAR(255)                                          NOT NULL,
+    restaurant_id BIGINT                                                NOT NULL,
+    poc_role      BIGINT                                                NOT NULL,
+    contact       VARCHAR(255)                                          NOT NULL,
+    created_by    BIGINT                                                NOT NULL,
+    created_at    TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at    TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    FOREIGN KEY (restaurant_id) REFERENCES public.restaurant (id),
+    FOREIGN KEY (created_by) REFERENCES public.kam_user (id)
+);
+
+-- Table: lead
+CREATE TABLE public.lead
+(
+    lead_id       BIGSERIAL PRIMARY KEY,
+    restaurant_id BIGINT                                                NOT NULL,
+    created_by    BIGINT                                                NOT NULL,
+    lead_status   BIGINT                                                NOT NULL,
+    created_at    TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at    TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    FOREIGN KEY (created_by) REFERENCES public.kam_user (id),
+    FOREIGN KEY (restaurant_id) REFERENCES public.restaurant (id)
+);
 
 
-CREATE INDEX lead_restaurant_id_idx ON public."lead" (restaurant_id);
-CREATE INDEX lead_created_by_idx ON public."lead" (created_by);
+-- Table: audit_change_log
+CREATE TABLE public.audit_change_log
+(
+    change_id   BIGSERIAL PRIMARY KEY,
+    entity_id   BIGINT                                                NOT NULL,
+    old_kam_id  BIGINT,
+    new_kam_id  BIGINT,
+    change_date TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    entity_type VARCHAR
+);
 
-CREATE INDEX restaurant_poc_restaurant_id_idx ON public.restaurant_poc (restaurant_id);
-CREATE INDEX restaurant_poc_created_by_idx ON public.restaurant_poc (created_by);
+-- Table: call_schedule
+CREATE TABLE public.call_schedule
+(
+    id                  BIGSERIAL PRIMARY KEY,
+    lead_id             BIGINT                      NOT NULL,
+    recurrence_type     VARCHAR(20)                 NOT NULL,
+    preferred_time      TIME WITHOUT TIME ZONE      NOT NULL,
+    start_date          TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+    end_date            TIMESTAMP WITHOUT TIME ZONE,
+    weekly_days         JSONB,
+    day_of_month        INT,
+    custom_day_interval INT,
+    last_call_date      TIMESTAMP WITHOUT TIME ZONE,
+    next_call_date      TIMESTAMP WITHOUT TIME ZONE,
+    time_zone           VARCHAR(50)                 NOT NULL,
+    created_at          TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    updated_at          TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    is_active           BOOLEAN                     DEFAULT TRUE,
+    FOREIGN KEY (lead_id) REFERENCES public.lead (lead_id)
+);
 
-ALTER TABLE public."order" ADD order_status varchar NOT NULL default 'PENDING';
+-- Table: interaction
+CREATE TABLE public.interaction
+(
+    id                  BIGSERIAL PRIMARY KEY,
+    caller_id           BIGINT                                                NOT NULL,
+    lead_id             BIGINT                                                NOT NULL,
+    restaurant_id       BIGINT                                                NOT NULL,
+    poc_id              BIGINT                                                NOT NULL,
+    call_schedule_id    BIGINT,
+    call_duration       NUMERIC                                                NOT NULL,
+    interaction_details TEXT                                                  NOT NULL,
+    interaction_type    VARCHAR(50)                                           NOT NULL,
+    created_at          TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    FOREIGN KEY (call_schedule_id) REFERENCES public.call_schedule (id),
+    FOREIGN KEY (caller_id) REFERENCES public.kam_user (id),
+    FOREIGN KEY (lead_id) REFERENCES public.lead (lead_id),
+    FOREIGN KEY (poc_id) REFERENCES public.restaurant_poc (poc_id),
+    FOREIGN KEY (restaurant_id) REFERENCES public.restaurant (id)
+);
 
-ALTER TABLE public."order" ALTER COLUMN cart_info DROP NOT NULL;
-ALTER TABLE public."order" ALTER COLUMN shipping_info DROP NOT NULL;
-ALTER TABLE public."order" ALTER COLUMN offer_details DROP NOT NULL;
+-- Table: kam_system_metrics
+CREATE TABLE public.kam_system_metrics
+(
+    id                BIGSERIAL PRIMARY KEY,
+    metric_name       VARCHAR(50)      NOT NULL,
+    metric_value      NUMERIC      NOT NULL,
+    metric_value_type VARCHAR(50) NOT NULL,
+    timeframe         VARCHAR(50) NOT NULL,
+    year              BIGINT      NOT NULL,
+    month             BIGINT      NULL,
+    day               BIGINT      NULL,
+    created_at        TIMESTAMP WITHOUT TIME ZONE DEFAULT NOW()
+);
+
+
+-- Table: orders
+CREATE TABLE public.orders
+(
+    id                  BIGSERIAL PRIMARY KEY,
+    order_id            UUID                                                   NOT NULL,
+    lead_id             BIGINT                                                 NOT NULL,
+    restaurant_id       BIGINT                                                 NOT NULL,
+    interaction_id      BIGINT                                                 NOT NULL,
+    restaurant_order_id VARCHAR(255)                                           NOT NULL,
+    amount              NUMERIC                                                 NOT NULL,
+    currency            VARCHAR(50)                                            NOT NULL,
+    cart_info           TEXT,
+    shipping_info       TEXT,
+    offer_details       TEXT,
+    created_by          BIGINT                                                 NOT NULL,
+    payment_methods     VARCHAR(50)                                            NOT NULL,
+    remarks             VARCHAR(255)                                           NOT NULL,
+    created_at          TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP  NOT NULL,
+    order_status        VARCHAR                     DEFAULT 'PENDING'::VARCHAR NOT NULL,
+    FOREIGN KEY (created_by) REFERENCES public.kam_user (id),
+    FOREIGN KEY (interaction_id) REFERENCES public.interaction (id),
+    FOREIGN KEY (lead_id) REFERENCES public.lead (lead_id),
+    FOREIGN KEY (restaurant_id) REFERENCES public.restaurant (id)
+);
+
+
+-- Indexes
+CREATE INDEX lead_created_by_idx ON public.lead USING btree (created_by);
+CREATE INDEX lead_restaurant_id_idx ON public.lead USING btree (restaurant_id);
+CREATE INDEX restaurant_poc_created_by_idx ON public.restaurant_poc USING btree (created_by);
+CREATE INDEX restaurant_poc_restaurant_id_idx ON public.restaurant_poc USING btree (restaurant_id);
+CREATE INDEX restaurant_created_by_idx ON public.restaurant USING btree (created_by);
+CREATE INDEX orders_lead_id_idx ON public.orders USING btree (lead_id);
+CREATE INDEX orders_restaurant_id_idx ON public.orders USING btree (restaurant_id);
+CREATE INDEX orders_interaction_id_idx ON public.orders USING btree (interaction_id);
+CREATE INDEX orders_order_id_idx ON public.orders USING btree (order_id,created_by);
