dd2c5dc92bcdf37b96dccefdf89c9b2cabb608fb
Keshav <keshav950personal@gmail.com>
2025-01-18 22:11:36 +0530

    Update generic error message

--- src/main/java/org/kamsystem/common/constants/DtoConstants.java
+++ src/main/java/org/kamsystem/common/constants/DtoConstants.java
@@ -3,7 +3,7 @@ package org.kamsystem.common.constants;
 public class DtoConstants {
 
     public static final String GENERIC_ERROR_MSG =
-        "System Error! We are experiencing technical difficulties & are working to fix them. Please try again later.";
+        "Server issue! We are facing some difficulties, please try after some time.";
 
     public static final String RESOURCE_NOT_FOUND = "Something went wrong! requested resource not found";
 
